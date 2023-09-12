e8a7af89e0744dda47059547377db22692e6d0c3
Helen Efebe <efebehelen95@gmail.com>
2023-09-12 14:08:42 +0100

    adds markup and components for hero section

---
+++ src/assets/images/index.js
@@ -0,0 +1,5 @@
+import logo from "@/assets/images/tv.png";
+import imdb from "@/assets/images/imdb.png";
+import rotten_tomatoes from "@/assets/images/rotten_tomatoes.png";
+
+export { logo, imdb, rotten_tomatoes };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import Image from "next/image";
-import logo from "@/assets/tv.png";
 import Seo from "@/components/Seo";
-import { MenuIcon } from "@/assets/Icons";
+import { MenuIcon, PlayIcon } from "@/assets/icons";
+import { imdb, logo, rotten_tomatoes } from "@/assets/images";
 
 export default function Home() {
   return (
     <>
       <Seo title="Home" />
-      {/* Header section */}
+      {/* Header */}
       <header className="p-5">
         <div className="flex justify-between items-center max-w-7xl mx-auto">
           {/* Branding */}
@@ -34,9 +34,43 @@ export default function Home() {
       </header>
 
       {/* Main content */}
-      <main></main>
+      <main>
+        {/* Hero section */}
+        <section>
+          <div className="flex justify-between items-center max-w-7xl mx-auto">
+            <div className="w-full max-w-md grid gap-4">
+              <h1 className="font-bold text-5xl">Movie title</h1>
 
-      {/* Footer section */}
+              <div className="flex items-center gap-10">
+                <div className="flex items-center gap-3">
+                  <Image alt="imdb" src={imdb} />
+                  <span>00/00</span>
+                </div>
+                <div className="flex items-center gap-3">
+                  <Image alt="rotten tomatoes" src={rotten_tomatoes} />
+                  <span>00%</span>
+                </div>
+              </div>
+
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                eu dui sit amet justo ullamcorper laoreet at sed nulla. Quisque
+                eget rutrum felis. Etiam sed mauris mi. In nec tortor nibh.
+                Mauris nec dui est.
+              </div>
+
+              <div>
+                <button className="flex items-center text-white bg-[#BE123C] rounded-md gap-2 px-4 py-[6px]">
+                  <PlayIcon />
+                  <span>Watch Trailer</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+
+      {/* Footer */}
       <footer></footer>
     </>
   );
