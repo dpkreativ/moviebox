3a81bcbda4652b0ea4028218fe265c122ec90f23
Divine Orji <dpkreativ@gmail.com>
2023-10-15 07:15:46 +0100

    add showcase.json file

--- src/assets/images/index.js
+++
@@ -1,6 +0,0 @@
-import logo from "@/assets/images/tv.png";
-import imdb from "@/assets/images/imdb.png";
-import rotten_tomatoes from "@/assets/images/rotten_tomatoes.png";
-import star from "@/assets/images/star.png";
-
-export { logo, imdb, rotten_tomatoes, star };
